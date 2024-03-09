fd51d36ab0e9502e50efaea3f23e7e5c0c9688ec
JTShocks <jacobdreyer2002@gmail.com>
2024-03-08 19:44:26 -0500

    Conveyor belt made
    They work pretty clean, player can move against them okay and the speed is variable as well as direction.
    
    You just need to rotate the root transform, but it all works.

---
+++ Assets/_Scripts/PlatformingElements/ConveyorBelt.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    [SerializeField] float beltSpeed;
+    [SerializeField] bool isBeltActive;
+    [SerializeField] bool beltMoveLeft;
+
+
+    void OnTriggerStay(Collider other)
+    {
+        PlayerController player = other.GetComponent<PlayerController>();
+        //If the player is in the trigger zone
+        if(player != null)
+        {
+            //Store the belt velocity, whatever it is set to based on the speed
+            Vector3 beltVelocity = transform.forward * beltSpeed;
+            if(beltMoveLeft)
+            {
+                beltVelocity = transform.forward * -1 * beltSpeed;
+            }
+
+            if(isBeltActive)
+            {
+            player.character.Move(beltVelocity * Time.fixedDeltaTime);
+            }
+
+            
+        }
+    }
+}
