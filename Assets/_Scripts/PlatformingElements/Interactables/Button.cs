7cbf777a11c7098484d15ec516b507876eaf4382
JTShocks <jacobdreyer2002@gmail.com>
2024-03-14 12:46:37 -0400

    Initial commits
    Setting up for the Button and the interactable objects

---
+++ Assets/_Scripts/PlatformingElements/Interactables/Button.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    //Place for the punchable button. Needs to specifically be triggered by the PunchAttack
+    //Interactable can have a reference to "triggered by dash punch"
+    //When the punch touches it, if it has that value set to true, it triggers the switch
+}
---
+++ Assets/_Scripts/PlatformingElements/Interactables/Interactable.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+   //Generic class for interactable objects, just to keep them in one place
+}
