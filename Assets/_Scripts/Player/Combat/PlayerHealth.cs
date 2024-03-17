29a7996688ff96e7b8d15981e804bdf4b839f0cc
JTShocks <jacobdreyer2002@gmail.com>
2024-03-16 21:05:13 -0400

    Update PlayerHealth.cs

--- Assets/_Scripts/Player/Combat/PlayerHealth.cs
+++ Assets/_Scripts/Player/Combat/PlayerHealth.cs
@@ -9,15 +9,5 @@ public class PlayerHealth : MonoBehaviour, ITakeDamage
         throw new System.NotImplementedException();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
