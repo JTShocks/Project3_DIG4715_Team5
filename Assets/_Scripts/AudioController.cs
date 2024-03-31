e55bf4006f28548ee1b3d48dad27ae840f1a4346
Kanewuu <wadehfla@gmail.com>
2024-03-31 18:00:43 -0400

    Added 2 sound sfx and some ambiance noise

---
+++ Assets/_Scripts/AudioController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+        void LateUpdate()
+        {
+            transform.rotation = Quaternion.Euler(Vector3.zero);
+        }
+    
+}
