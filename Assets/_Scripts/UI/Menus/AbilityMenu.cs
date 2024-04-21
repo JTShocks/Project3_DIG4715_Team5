59f840349eb7d4340697752ffa94ddbc11a2feb4
JTShocks <jacobdreyer2002@gmail.com>
2024-04-21 15:01:31 -0400

    Continuing work on the menus
    Added some starting animations, just to figure out how everything is spaced out

---
+++ Assets/_Scripts/UI/Menus/AbilityMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityMenu : MonoBehaviour
+{
+    //Like the ability UI, creating a small list for the current equipped abilities
+
+    //Clicking on the button = fan out the options for that specific branch.
+
+    //Get the current equipped abilities and their images
+}
