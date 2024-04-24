5c5cb43fef2c40b3c8940305462d997007bf40bf
SurgeHavoc <87490063+SurgeHavoc@users.noreply.github.com>
2024-04-24 16:27:20 -0400

    ...

---
+++ Assets/_Scripts/PlatformingElements/RotateGear.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGear : MonoBehaviour
+{
+    public float rotationSpeed = -90.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+    }
+}
